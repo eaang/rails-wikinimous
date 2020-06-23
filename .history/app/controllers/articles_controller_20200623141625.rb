1439e43580f3cd448806d66b89c521f531efcbad
Evan Ang <evan.a.ang@gmail.com>
2020-06-23 14:20:00 +0800

    all functioning

--- .history/app/controllers/articles_controller_20200623141625.rb
+++ .history/app/controllers/articles_controller_20200623141625.rb
@@ -9,29 +9,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new
-  end
-
-  def create
-    @article = Article.new(article_params)
-    @article.save
-
-    redirect_to articles_path
-  end
-
-  def edit
-  end
-
-  def update
-    @article.update(article_params)
-
-    redirect_to article_path(@article)
-  end
-
-  def destroy
-    @article.destroy
-
-    redirect_to articles_path
+    @task = Task.new
   end
 
   private
--- .history/app/controllers/articles_controller_20200623141651.rb
+++ .history/app/controllers/articles_controller_20200623141651.rb
@@ -12,28 +12,6 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def create
-    @article = Article.new(article_params)
-    @article.save
-
-    redirect_to articles_path
-  end
-
-  def edit
-  end
-
-  def update
-    @article.update(article_params)
-
-    redirect_to article_path(@article)
-  end
-
-  def destroy
-    @article.destroy
-
-    redirect_to articles_path
-  end
-
   private
 
   def article_params
